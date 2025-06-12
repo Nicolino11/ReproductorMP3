7046644f98f0a84865d35ca5a92f490fa0fb5532
Nicolás Raposo <nicolasraposo11@gmail.com>
2025-06-12 12:13:05 -0300

    Añado comentarios a los errores en la TaskC

--- components/mi_task_c/mi_task_c.c
+++ components/mi_task_c/mi_task_c.c
@@ -3,11 +3,8 @@
 #include "freertos/semphr.h"
 #include "include/mi_task_c.h"
 #include "esp_log.h"
-#include "esp_timer.h"
-#include "../led_strip/include/led_strip.h"
-#include "../mi_delay/include/mi_delay.h"
-#include "../mi_queue/include/mi_queue.h"
-// NO incluir mi_task_a.h aquí porque ya está incluido en mi_task_c.h
+#include "esp_timer.h" //--> Esta libreria no deberia hacer falta
+#include "../mi_queue/include/mi_queue.h" //--> Hay alguna razon para incluir las librerias con este formato?
 
 // Variables globales
 static led_color_t *shared_color = NULL;
@@ -18,8 +15,7 @@ static void vTaskC(void *pvParameters)
     QueueHandle_t queue = (QueueHandle_t)pvParameters;
     mi_evento_t evento;
 
-    led_strip_t *strip;
-    led_rgb_init(&strip);
+    //--> Saqué el uso del led porque esta task no tiene que usarlo
 
     while (1)
     {
@@ -42,24 +38,17 @@ static void vTaskC(void *pvParameters)
                 }
             }
 
-            // Aplicar el color al LED
-            if (evento.r > 0 || evento.g > 0 || evento.b > 0) {
-                turn_led_on(strip, evento.r, evento.g, evento.b);
-            } else {
-                turn_led_off(strip);
-            }
-
             // Manejar el tiempo de espera si es necesario
             if (evento.tiempo_ms > 0)
-            {
+            {   //--> Porque hay un semaforo para el timer? la taskC es la unica que lo usa no deberia necesitar sincronizacion
                 SemaphoreHandle_t timer_semaphore = xSemaphoreCreateBinary();
-
+                
                 void timer_callback(void *arg)
                 {
                     SemaphoreHandle_t *sem = (SemaphoreHandle_t *)arg;
                     xSemaphoreGive(*sem);
                 }
-
+                //--> El timer tiene que ser el de freeRTOS, no el de ESP
                 const esp_timer_create_args_t timer_args = {
                     .callback = &timer_callback,
                     .arg = (void *)&timer_semaphore,
@@ -70,10 +59,11 @@ static void vTaskC(void *pvParameters)
                 esp_timer_start_once(timer, evento.tiempo_ms * 1000); // Convertir a us
 
                 xSemaphoreTake(timer_semaphore, portMAX_DELAY);
-
+                //--> otra vez, porque el semaforo?
                 vSemaphoreDelete(timer_semaphore);
                 esp_timer_stop(timer);
                 esp_timer_delete(timer);
+                //--> de vuelta el timer tiene que ser el de freertos
             }
         }
     }
@@ -88,6 +78,6 @@ void task_c_set_shared_resources(led_color_t *color, SemaphoreHandle_t mutex)
 task_handle_t mi_task_c_start(QueueHandle_t queue)
 {
     TaskHandle_t handle = NULL;
-    xTaskCreate(vTaskC, "ConsumidorC", 3000, (void *)queue, 2, &handle);
+    xTaskCreate(vTaskC, "ConsumidorC", 3072, (void *)queue, 2, &handle);
     return handle;
 }
